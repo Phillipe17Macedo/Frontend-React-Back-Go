14f793e6b47e6d6494916c080085854e32307a9c
Phillipe17Macedo <ph.stos.udia@gmail.com>
2024-08-30 17:00:27 -0300

    update-packages-v0.2

---
+++ build/precache-manifest.c8f8224d65b7421bb70fc7feea2f4083.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a54e8d2591036ae0324e7ac86cad68b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d2a4af3bf05f6cadbb7",
+    "url": "/static/css/2.b71989ea.chunk.css"
+  },
+  {
+    "revision": "2423a68c76dd3b5cf08f",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9d2a4af3bf05f6cadbb7",
+    "url": "/static/js/2.7e3a3e3f.chunk.js"
+  },
+  {
+    "revision": "cf5bad70b64104c728026f4ce2304e20",
+    "url": "/static/js/2.7e3a3e3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "178581334fa3e882e978",
+    "url": "/static/js/3.50f9ff74.chunk.js"
+  },
+  {
+    "revision": "2423a68c76dd3b5cf08f",
+    "url": "/static/js/main.1a39ca7e.chunk.js"
+  },
+  {
+    "revision": "5ad98477a9c6ce8bd705",
+    "url": "/static/js/runtime-main.3caa8fc8.js"
+  }
+]);
